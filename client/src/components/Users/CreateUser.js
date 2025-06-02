ccc04891be847e85047491da30d7d6f2c822c961
skvizyyy <ivan.shukaev280210@gmail.com>
2025-06-02 19:59:50 +0300

    Реализовали модальное окно ErrorModal.

--- client/src/components/Users/CreateUser.js
+++ client/src/components/Users/CreateUser.js
@@ -2,18 +2,28 @@ import { useState } from 'react'
 import Button from '../UI/Button';
 import Card from '../UI/Card';
 import styles from './CreateUser.module.css'
+import ErrorModal from '../UI/ErrorModal';
 
-const CreateUser = () => {
+const CreateUser = (props) => {
     const [inputName, setInputName] = useState('')
     const [inputAge, setInputAge] = useState('')
+    const [error, setError] = useState('')
 
     const createUserHandler = (event) => {
         event.preventDefault()
         console.log(inputName, inputAge)
-        if(inputName.trim().length === 0) {
+        if (inputName.trim().length === 0) {
+            setError({
+                title: 'Некорректный ввод',
+                message: 'Поля ввода не могут быть пустыми!'
+            })
             return
         }
-        if(+inputAge < 1 || +inputAge > 100) {
+        if (+inputAge < 1 || +inputAge > 100) {
+            setError({
+                title: 'Неккоректный возраст',
+                message: 'Возраст должен быть больше ноля!'
+            })
             return
         }
         props.onCreateUser(inputName, inputAge)
@@ -31,18 +41,23 @@ const CreateUser = () => {
     }
 
     return (
-        <Card className={styles.input}>
-            <form onSubmit={createUserHandler}>
-                <label htmlFor="name">Имя</label>
-                <input id='name' type='text' onChange={nameChangeHandler} value={inputName} />
+        <>
+            {error
+                && <ErrorModal title={error.title}
+                    message={error.message}></ErrorModal>}
+            <Card className={styles.input}>
+                <form onSubmit={createUserHandler}>
+                    <label htmlFor="name">Имя</label>
+                    <input id='name' type='text' onChange={nameChangeHandler} value={inputName} />
 
-                <label htmlFor="age">Возраст</label>
-                <input id='age' type='number' onChange={ageChangeHandler} value={inputAge} />
+                    <label htmlFor="age">Возраст</label>
+                    <input id='age' type='number' onChange={ageChangeHandler} value={inputAge} />
 
-                {/* <button type='submit'>Добавить пользователя</button> */}
-                <Button type="submit">Добавить пользователя</Button>
-            </form>
-        </Card>
+                    {/* <button type='submit'>Добавить пользователя</button> */}
+                    <Button type="submit">Добавить пользователя</Button>
+                </form>
+            </Card>
+        </>
 
     )
 
